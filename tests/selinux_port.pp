abc51e313606bb4b8746ef716534379ee53567aa
Jeff Goldschrafe <jeff@holyhandgrenade.org>
2013-05-20 16:29:38 -0400

    Standardize code style in tests

--- tests/selinux_port.pp
+++ tests/selinux_port.pp
@@ -1,14 +1,14 @@
 # Make sure you have semanage command available
 # sudo puppet apply --noop --modulepath='..' tests/selinux_port.pp
 
-selinux_port {'udp/53':
-  seltype => 'dns_port_t'
+selinux_port { 'udp/53':
+  seltype => 'dns_port_t',
 }
 
-selinux_port {'tcp/8080-8085':
-  seltype => 'http_port_t'
+selinux_port { 'tcp/8080-8085':
+  seltype => 'http_port_t',
 }
 
-resources {'selinux_port':
-	purge => false,
+resources { 'selinux_port':
+  purge => false,
 }
